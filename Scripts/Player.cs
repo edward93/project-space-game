df0ee96d10ae270a3c12546fa4252f4fee82ae84
edward93 <edward.chopuryan@gmail.com>
2024-04-09 23:48:59 -0400

    changes player from CharacterBody2D to RigidBody2D

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,10 +3,8 @@ using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-public partial class Player : CharacterBody2D
+public partial class Player : RigidBody2D
 {
-  [Export]
-  public float Speed = 40.0f;
   /// <summary>
   /// Instant acceleration of the object
   /// </summary>
@@ -22,7 +20,7 @@ public partial class Player : CharacterBody2D
   /// How quickly model rotates
   /// </summary>
   [Export]
-  public float RotationAccelerationFactor = 2.0f;
+  public float RotationAccelerationFactor = 1.3f;
 
   /// <summary>
   /// Max length of the aim helper line
@@ -46,7 +44,6 @@ public partial class Player : CharacterBody2D
   /// </summary>
   private Line2D _aimLine;
 
-  private Line2D _forwardDir;
 
   /// <summary>
   /// Init
@@ -55,7 +52,6 @@ public partial class Player : CharacterBody2D
   {
     _model = GetNode<Node2D>("Model");
     _aimLine = GetNode<Line2D>("HelperGuides/Aim");
-    _forwardDir = GetNode<Line2D>("HelperGuides/Forward");
 
     _aimLine.AddPoint(Vector2.Zero);
     _aimLine.AddPoint(Vector2.Zero);
@@ -67,29 +63,6 @@ public partial class Player : CharacterBody2D
   /// <param name="inputEvent">Input event</param>
   public override void _Input(InputEvent inputEvent)
   {
-    if (inputEvent is InputEventMouseMotion)
-    {
-      // var mouseMovement = inputEvent as InputEventMouseMotion;
-
-      // _aimLine.SetPointPosition(1, ToLocal(mouseMovement.Position));
-
-      // GD.Print($" aim line start: {_aimLine.GetPointPosition(0)} End: {_aimLine.GetPointPosition(1)}");
-
-      // _model.Rotation = Mathf.LerpAngle(_model.Rotation, modelDir.AngleTo(mouseMovement.GlobalPosition - _model.GlobalPosition), RotationAccelerationFactor);
-      // _model.Rotate(modelDir.AngleTo(mouseMovement.GlobalPosition - _model.GlobalPosition));
-
-      // _model.GlobalRotation = Mathf.LerpAngle(_model.GlobalRotation, _model.GetAngleTo(mouseMovement.GlobalPosition) + Mathf.Pi/2, RotationAccelerationFactor);
-
-      // Rotate(GetAngleTo(mouseMovement.GlobalPosition) + Mathf.Pi/2);
-      // very simple way of rotating
-      // LookAt(mouseMovement.GlobalPosition);
-
-      // GD.Print(Mathf.RadToDeg(_model.GetAngleTo(mouseMovement.GlobalPosition)));
-      // GD.Print($"Angle: {Mathf.RadToDeg(modelDir.AngleTo(mouseMovement.GlobalPosition - _model.GlobalPosition))}");
-      // // mouseMovement.Position
-      // GD.Print($"Vector pointing from player to mouse: {mouseMovement.GlobalPosition - _model.GlobalPosition}");
-      // GD.Print($"_model Global Rotation: {_model.Rotation:F3} mouse Global Rotation: ({mouseMovement.GlobalPosition.X:F3}, {mouseMovement.GlobalPosition.Y:F3})");
-    }
     if (inputEvent.IsActionPressed("Esc"))
     {
       GD.Print($"Esc pressed - GameState: {GetTree().Paused}");
@@ -98,21 +71,23 @@ public partial class Player : CharacterBody2D
     }
   }
 
+  /// <summary>
+  /// Physics process
+  /// </summary>
+  /// <param name="delta"></param>
   public override void _PhysicsProcess(double delta)
   {
-    // TODO: rotate player gradually
-    LookAt(GetGlobalMousePosition());
-
     // RealisticAbsoluteControls(delta);
     RealisticRelativeControls(delta);
-    MoveAndSlide();
   }
 
+  /// <summary>
+  /// Frame process
+  /// </summary>
+  /// <param name="delta"></param>
   public override void _Process(double delta)
   {
     DrawAimLine(delta);
-
-    // _forwardDir.SetPointPosition(1, _forwardDir.Points[0] + (Transform.X - _forwardDir.Points[0]).Normalized() * MaxAimLength );
   }
 
   /// <summary>
@@ -130,12 +105,19 @@ public partial class Player : CharacterBody2D
     var acceleration = movementDir * Acceleration;
 
     // apply dampening (Friction)
-    acceleration = acceleration.Lerp(-Velocity, (float)delta * DampeningFactor);
+    // acceleration = acceleration.Lerp(-Velocity, (float)delta * DampeningFactor);
+    acceleration = acceleration.Lerp(-LinearVelocity, (float)delta * DampeningFactor);
 
     // update the Velocity v = a*t (where t = 1 since we are running this per physics tic)
-    Velocity += acceleration;
+    // Velocity += acceleration;
+    // LinearVelocity += acceleration;
+    ApplyCentralForce(acceleration);
   }
 
+  /// <summary>
+  /// Realistic relative controls for Rigid body
+  /// </summary>
+  /// <param name="delta"></param>
   private void RealisticRelativeControls(double delta)
   {
     // read input
@@ -144,12 +126,47 @@ public partial class Player : CharacterBody2D
     var movementDir = direction.Normalized();
     // calc target acceleration (assumes near instant torque, meaning object reaches the target acceleration instantly)
     var acceleration = movementDir.Rotated(Rotation) * Acceleration;
-    // GD.Print($"Rotated dir: {movementDir.Rotated(Rotation)}");
+
+    // rotate the node only when thurs is applied
+    if (movementDir != Vector2.Zero) RotateNode(delta);
+
     // apply dampening (Friction)
-    acceleration = acceleration.Lerp(-Velocity, (float)delta * DampeningFactor);
+    acceleration = acceleration.Lerp(-LinearVelocity, (float)delta * DampeningFactor);
     // update the Velocity v = a*t (where t = 1 since we are running this per physics tic)
-    Velocity += acceleration;
+    LinearVelocity += acceleration;
   }
+
+  /// <summary>
+  /// Slowly rotates the node towards the aim point
+  /// </summary>
+  /// <param name="delta"></param>
+  private void RotateNode(double delta)
+  {
+    // calc how much to rotate
+    var rotation = GetAngleTo(ToGlobal(_aimLine.GetPointPosition(1)));
+
+    // slowly rotate the root node
+    Rotation = Mathf.Lerp(Rotation, Rotation + rotation, (float)delta * RotationAccelerationFactor);
+  }
+
+  /// <summary>
+  /// Realistic relative controls for character body
+  /// </summary>
+  /// <param name="delta">delta time</param>
+  // private void RealisticRelativeControlsCharacterBody(double delta)
+  // {
+  //   // read input
+  //   var direction = Input.GetVector("Thrust Down", "Thrust Up", "MoveLeft", "MoveRight");
+  //   // calc movement dir
+  //   var movementDir = direction.Normalized();
+  //   // calc target acceleration (assumes near instant torque, meaning object reaches the target acceleration instantly)
+  //   var acceleration = movementDir.Rotated(Rotation) * Acceleration;
+  //   // GD.Print($"Rotated dir: {movementDir.Rotated(Rotation)}");
+  //   // apply dampening (Friction)
+  //   acceleration = acceleration.Lerp(-Velocity, (float)delta * DampeningFactor);
+  //   // update the Velocity v = a*t (where t = 1 since we are running this per physics tic)
+  //   Velocity += acceleration;
+  // }
 
   /// <summary>
   /// Draws the aim line
@@ -178,20 +195,5 @@ public partial class Player : CharacterBody2D
     _aimLine.SetPointPosition(1, endPoint);
     // update start position
     _aimLine.SetPointPosition(0, startPoint);
-  }
-
-  /// <summary>
-  /// More arcade like velocity/acceleration controls (const acceleration)
-  /// </summary>
-  /// <param name="movementDir">Direction of the movement (comes from the controller)</param>
-  /// <param name="delta">Processing time</param>
-  private void ArcadeControls(Vector2 movementDir, double delta)
-  {
-    // calc acceleration
-    var acceleration = (float)delta * Acceleration;
-    // calc target velocity
-    var targetVelocity = movementDir * Speed;
-    // update velocity
-    Velocity = Velocity.MoveToward(targetVelocity, acceleration);
   }
 }
