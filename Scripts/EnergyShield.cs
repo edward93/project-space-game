d7a3573e8f306c4c651ad2af20435c0e2d14fde9
edward93 <edward.chopuryan@gmail.com>
2024-04-20 21:07:44 -0400

    extract energy shield scene

---
+++ Scripts/EnergyShield.cs
@@ -0,0 +1,82 @@
+using Godot;
+using System;
+
+public partial class EnergyShield : Node2D
+{
+
+  /// <summary>
+  /// Shield collider
+  /// </summary>
+  private CollisionShape2D _shieldCollider;
+
+  /// <summary>
+  /// Shield texture/sprite
+  /// </summary>
+  private Sprite2D _shieldSprite;
+
+  /// <summary>
+  /// Shield is enabled or not
+  /// </summary>
+  private bool _shieldEnabled = false;
+
+  /// <summary>
+  /// Init
+  /// </summary>
+  public override void _Ready()
+  {
+    _shieldSprite = GetNode<Sprite2D>("ShieldTexture");
+    _shieldCollider = GetNode<CollisionShape2D>("Shield/ShieldCollider");
+    _shieldEnabled = false;
+
+
+  }
+  /// <summary>
+  /// Handle input events
+  /// </summary>
+  /// <param name="inputEvent">Input event</param>
+  public override void _Input(InputEvent inputEvent)
+  {
+    if (inputEvent.IsActionPressed("Toggle Shield"))
+    {
+      HandleShiedInput();
+    }
+  }
+
+  /// <summary>
+  /// Frame process
+  /// </summary>
+  /// <param name="delta"></param>
+  public override void _Process(double delta)
+  {
+  }
+
+  /// <summary>
+  /// Handles shield toggle
+  /// </summary>
+  private void HandleShiedInput()
+  {
+    // toggle shield
+    if (_shieldEnabled) DisableShield();
+    else EnableShield();
+    // toggle shield
+    _shieldEnabled = !_shieldEnabled;
+  }
+
+  /// <summary>
+  /// Enables the shield
+  /// </summary>
+  private void EnableShield()
+  {
+    _shieldCollider.Disabled = false;
+    _shieldSprite.Visible = false;
+  }
+
+  /// <summary>
+  /// Disable the shield
+  /// </summary>
+  private void DisableShield()
+  {
+    _shieldCollider.Disabled = true;
+    _shieldSprite.Visible = true;
+  }
+}
